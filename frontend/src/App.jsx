58e659cb676f4079b6a27782bac3390d94a6e7c6
Sagar Soradi <sagarsordi011@gmail.com>
2025-07-20 15:28:16 +0530

    Set up fronted with vite/react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-slate-900 w-auto h-auto">
+      <h1 className="text-3xl">Welcome to To-Do web app</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
