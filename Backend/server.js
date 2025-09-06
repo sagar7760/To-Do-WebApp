7637ff3e002102387840bf5e80efd929b8bb1a37
Sagar Soradi <sagarsordi011@gmail.com>
2025-09-06 21:06:11 +0530

    updated cors origin for frontend

--- Backend/server.js
+++ Backend/server.js
@@ -84,7 +84,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://127.0.0.1:5173',
   'http://127.0.0.1:5174',
-  'https://to-do-web-app-hazel.vercel.app', // Your Vercel frontend URL
+  'https://talkly-coral.vercel.app/', // Your Vercel frontend URL
 ];
 if (process.env.FRONTEND_URL) {
   allowedOrigins.push(process.env.FRONTEND_URL);
